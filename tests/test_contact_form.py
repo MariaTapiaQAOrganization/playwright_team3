51916887b212af5461d250108f7bd7a22bcf7974
Maria Tapia <mariatm89@yahoo.es>
2026-05-02 13:12:00 +0200

    tests contact

--- tests/test_contact_form.py
+++ tests/test_contact_form.py
@@ -1,5 +1,6 @@
 
 from playwright.sync_api import expect
+from pytest_playwright.pytest_playwright import page
 
 
 def test_contact_required_message_empty(page):
@@ -11,3 +12,43 @@ def test_contact_required_message_empty(page):
 
     expect(page.get_by_text("El mensaje es obligatorio")).to_be_visible()
     
+
+def test_contac_submit_the_form_with_all_required_fields_filled_out(page):
+    print("Given the user open contact web Contáctanos | Vida Verde" )
+    page.goto("https://web-qa.dev.adalab.es/contact")
+    print ("When the user enters Marta Diaz in the name field")
+    page.get_by_role("textbox", name="Nombre *").fill("Marta Diaz")
+    print ("And the user enters test_automation@test.com in the email field")
+    page.get_by_role("textbox", name="Email *").fill("test_automation@test.com")
+    print("And enters Test message in the message field")
+    page.get_by_role("textbox", name="Mensaje *").fill("Test message")
+    print("And submit the form")
+    page.get_by_role("button", name="Enviar Mensaje").click()
+    print ("Then the user should see the message 'Message sent'")
+    expect(page.get_by_text("¡Mensaje enviado con éxito!")).to_be_visible()
+
+
+def test_contact_submit_the_form_with_required_name_field_left_empty(page):
+    print("Given the user open contact web Contáctanos | Vida Verde" )
+    page.goto("https://web-qa.dev.adalab.es/contact")
+    print("When the user enters test_automation@test.com in the email field")
+    page.get_by_role("textbox", name="Email *").fill("test_automation@test.com")
+    print("And enters Test message in the message field")
+    page.get_by_role("textbox", name="Mensaje *").fill("Test message")
+    print("And submit the form")
+    page.get_by_role("button", name="Enviar Mensaje").click()
+    print ("Then the user should see the error message 'The name is required'")   
+    expect(page.get_by_text("El nombre es obligatorio")).to_be_visible()
+
+def test_contact_submit_the_form_with_required_email_field_left_empty(page):
+    print("Given the user open contact web Contáctanos | Vida Verde" )
+    page.goto("https://web-qa.dev.adalab.es/contact")
+    print ("When the user enters Marta Diaz in the name field")
+    page.get_by_role("textbox", name="Nombre *").fill("Marta Diaz")
+    print("And enters Test message in the message field")
+    page.get_by_role("textbox", name="Mensaje *").fill("Test message")
+    print("And submit the form")
+    page.get_by_role("button", name="Enviar Mensaje").click()
+    print ("Then the user should see the error message 'The email is required'")   
+    expect(page.get_by_text("El email es obligatorio")).to_be_visible()
+
