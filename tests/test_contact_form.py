99a8868a2faec1c3a3dd69572d55980760f8d25f
Maria Tapia <mariatm89@yahoo.es>
2026-05-04 19:44:45 +0200

    corrección

--- tests/test_contact_form.py
+++ tests/test_contact_form.py
@@ -1,9 +1,9 @@
 
 import pytest
-from playwright.sync_api import expect
+from playwright.sync_api import Page, expect
 
 
-def test_contact_required_message_empty(page):
+def test_contact_required_message_empty(page: Page):
     page.goto("https://web-qa.dev.adalab.es/")
     page.get_by_role("link", name="Contacto").click()
     page.get_by_role("textbox", name="Nombre *").fill("Marta Diaz")
@@ -14,7 +14,7 @@ def test_contact_required_message_empty(page):
 
 
 @pytest.mark.skip(reason="Skipped due to known bug KAN-15: Contact page accepts invalid email formats and submits the form successfully - https://equipo3qa.atlassian.net/browse/KAN-15")
-def test_contact_invalid_email(page):
+def test_contact_invalid_email(page: Page):
     page.goto("https://web-qa.dev.adalab.es/")
     page.get_by_role("link", name="Contacto").click()
     page.get_by_role("textbox", name="Nombre *").fill("Marta Diaz")
@@ -25,7 +25,7 @@ def test_contact_invalid_email(page):
     expect(page.get_by_text("¡Mensaje enviado con éxito!")).not_to_be_visible()
 
 
-def test_contact_submit_the_form_with_all_required_fields_filled_out(page):
+def test_contact_submit_the_form_with_all_required_fields_filled_out(page: Page):
     print("Given user visit contact page")  
     page.goto("https://web-qa.dev.adalab.es/contact")
     print("When the user enters Marta Diaz in the name field")
@@ -40,7 +40,7 @@ def test_contact_submit_the_form_with_all_required_fields_filled_out(page):
     expect(page.get_by_text("¡Mensaje enviado con éxito!")).to_be_visible()
 
 
-def test_contact_submit_the_form_with_required_name_field_left_empty(page):
+def test_contact_submit_the_form_with_required_name_field_left_empty(page: Page):
     print("Given user visit contact page")
     page.goto("https://web-qa.dev.adalab.es/contact")
     print("When the user enters test_automation@test.com in the email field")
@@ -53,7 +53,7 @@ def test_contact_submit_the_form_with_required_name_field_left_empty(page):
     expect(page.get_by_text("El nombre es obligatorio")).to_be_visible()
 
 @pytest.mark.skip(reason="Contact page - the form can be submitted even when the email field is left empty- https://equipo3qa.atlassian.net/browse/KAN-13")
-def test_contact_submit_the_form_with_required_email_field_left_empty(page):
+def test_contact_submit_the_form_with_required_email_field_left_empty(page: Page):
     print("Given user visit contact page")
     page.goto("https://web-qa.dev.adalab.es/contact")
     print("When the user enters Marta Diaz in the name field")
